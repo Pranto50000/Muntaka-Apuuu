5ffd3aff02dc93f41979bd9e695a8c589c6c2e5f
Pranto Kumar Sarker <90765689+Pranto50000@users.noreply.github.com>
2026-08-26 22:44:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: '28, August, 20225',
+  birthdate: '28, August, 2025',
   name: 'Sadika Alam'
 };
 
